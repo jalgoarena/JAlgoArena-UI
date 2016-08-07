22e00c1576fd4dbc2bd8af8cc93e587a52272957
Jacek Spólnik <jacek.spolnik@gmail.com>
2016-08-07 18:59:52 +0200

    - clean up default values from spin.js settings

--- public/scripts/bundle.js
+++ public/scripts/bundle.js
@@ -22321,26 +22321,15 @@
 	        key: 'componentDidMount',
 	        value: function componentDidMount() {
 	            var opts = {
-	                lines: 13, // The number of lines to draw
-	                length: 20, // The length of each line
-	                width: 10, // The line thickness
-	                radius: 30, // The radius of the inner circle
-	                corners: 1, // Corner roundness (0..1)
-	                rotate: 0, // The rotation offset
-	                direction: 1, // 1: clockwise, -1: counterclockwise
-	                color: '#000', // #rgb or #rrggbb or array of colors
-	                speed: 1, // Rounds per second
-	                trail: 60, // Afterglow percentage
-	                shadow: false, // Whether to render a shadow
-	                hwaccel: false, // Whether to use hardware acceleration
-	                className: 'spinner', // The CSS class to assign to the spinner
-	                zIndex: 2e9, // The z-index (defaults to 2000000000)
-	                top: 'auto', // Top position relative to parent in px
-	                left: 'auto' // Left position relative to parent in px
+	                lines: 13,
+	                length: 20,
+	                width: 10,
+	                radius: 30,
+	                trail: 60
 	            };
 
 	            var target = document.getElementById('searching_spinner_center');
-	            var spinner = new _spin2.default(opts).spin(target);
+	            new _spin2.default(opts).spin(target);
 	        }
 	    }, {
 	        key: 'render',
--- src/components/SubmissionInProgressSpinner.jsx
+++ src/components/SubmissionInProgressSpinner.jsx
@@ -4,26 +4,15 @@ import Spinner from 'spin.js';
 export default class SubmissionInProgressSpinner extends React.Component {
     componentDidMount() {
         const opts = {
-            lines: 13, // The number of lines to draw
-            length: 20, // The length of each line
-            width: 10, // The line thickness
-            radius: 30, // The radius of the inner circle
-            corners: 1, // Corner roundness (0..1)
-            rotate: 0, // The rotation offset
-            direction: 1, // 1: clockwise, -1: counterclockwise
-            color: '#000', // #rgb or #rrggbb or array of colors
-            speed: 1, // Rounds per second
-            trail: 60, // Afterglow percentage
-            shadow: false, // Whether to render a shadow
-            hwaccel: false, // Whether to use hardware acceleration
-            className: 'spinner', // The CSS class to assign to the spinner
-            zIndex: 2e9, // The z-index (defaults to 2000000000)
-            top: 'auto', // Top position relative to parent in px
-            left:'auto' // Left position relative to parent in px
+            lines: 13,
+            length: 20,
+            width: 10,
+            radius: 30,
+            trail: 60
         };
 
         let target = document.getElementById('searching_spinner_center');
-        let spinner = new Spinner(opts).spin(target);
+        new Spinner(opts).spin(target);
     }
 
     render() {
