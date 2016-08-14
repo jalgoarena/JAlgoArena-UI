8bc863a73c3546680cb55b81a41786adaae9ee6e
Jacek Spólnik <jacek.spolnik@gmail.com>
2016-08-14 16:49:17 +0200

    - moving to react bootstrap, part 4 (move Navigation)

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,6 +1,7 @@
 "use strict";
 import React from "react";
 import {Link} from "react-router";
+import {Navbar, Nav, NavItem} from "react-bootstrap";
 
 export default class Menu extends React.Component {
     render() {
@@ -14,27 +15,19 @@ export default class Menu extends React.Component {
             color: "#3c763d"
         };
 
-        return <nav className="navbar navbar-default navbar-fixed-top">
-            <div className="container-fluid">
-                <div className="navbar-header">
-                    <button type="button" className="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar"
-                            aria-expanded="false" aria-controls="navbar">
-                        <span className="sr-only">Toggle navigation</span>
-                        <span className="icon-bar"> </span>
-                        <span className="icon-bar"> </span>
-                        <span className="icon-bar"> </span>
-                    </button>
-                    <a className="navbar-brand" href="/"><img src="img/logo.png" style={logoStyle} /></a>
-                </div>
-                <div id="navbar" className="navbar-collapse collapse">
-                    <ul className="nav navbar-nav navbar-right">
-                        <li><Link activeStyle={activeStyle} to="/"><i className="fa fa-home fa-lg"> </i> Home</Link></li>
-                        <li><Link activeStyle={activeStyle} to="/problems"><i className="fa fa-lightbulb-o fa-lg"> </i> Problems</Link></li>
-                        <li><Link activeStyle={activeStyle} to="/leaderboard"><i className="fa fa-trophy fa-lg"> </i> Leaderboard</Link></li>
-                        <li><Link activeStyle={activeStyle} to="/profile"><i className="fa fa-user fa-lg"> </i> Profile</Link></li>
-                    </ul>
-                </div>
-            </div>
-        </nav>;
+        return <Navbar fixedTop fluid>
+            <Navbar.Header>
+                <Navbar.Toggle/>
+                <a className="navbar-brand" href="/"><img src="img/logo.png" style={logoStyle} /></a>
+            </Navbar.Header>
+            <Navbar.Collapse>
+                <Nav role="navigation" eventKey={0} pullRight>
+                    <li><Link activeStyle={activeStyle} to="/"><i className="fa fa-home fa-lg"> </i> Home</Link></li>
+                    <li><Link activeStyle={activeStyle} to="/problems"><i className="fa fa-lightbulb-o fa-lg"> </i> Problems</Link></li>
+                    <li><Link activeStyle={activeStyle} to="/leaderboard"><i className="fa fa-trophy fa-lg"> </i> Leaderboard</Link></li>
+                    <li><Link activeStyle={activeStyle} to="/profile"><i className="fa fa-user fa-lg"> </i> Profile</Link></li>
+                </Nav>
+            </Navbar.Collapse>
+        </Navbar>;
     }
 }
