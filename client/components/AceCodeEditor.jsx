96cc80da75ef549aa3f3949f8cbafdb6bd272437
Jacek Spólnik <jacek.spolnik@gmail.com>
2016-08-31 20:21:20 +0200

    - prohibit to paste ready to go solutions

--- client/components/AceCodeEditor.jsx
+++ client/components/AceCodeEditor.jsx
@@ -1,11 +1,16 @@
 import React from 'react';
 import 'brace';
 import AceEditor from 'react-ace';
+import {Alert} from 'react-bootstrap';
 
 import 'brace/mode/java';
 import 'brace/theme/tomorrow_night_eighties';
 
 export default class AceCodeEditor extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {isCheating: false};
+    }
     componentDidMount() {
         this.props.onSourceCodeChanged(this.props.sourceCode);
     }
@@ -15,6 +20,15 @@ export default class AceCodeEditor extends React.Component {
             marginBottom: 10
         };
 
+        if (this.state.isCheating) {
+            return <div style={editorStyle}>
+                <Alert bsStyle="danger">
+                    <h4>Oh snap! You are cheating!</h4>
+                    <p>Please refresh page to continue and please stop pasting ready to go code. Be fair, as it is supposed to be fun and fair game.</p>
+                </Alert>
+            </div>;
+        }
+
         return <div style={editorStyle}>
             <AceEditor
                 mode="java"
@@ -22,6 +36,9 @@ export default class AceCodeEditor extends React.Component {
                 name="editor"
                 value={this.props.sourceCode}
                 onChange={this.props.onSourceCodeChanged}
+                onPaste={() => {
+                    this.setState({isCheating: true});
+                }}
                 height="350px"
                 width="100%"
                 editorProps={{$blockScrolling: true}}
