c7e27ee65a43cdfa2599bb7808273b9e2a151f39
Jacek Spólnik <jacek.spolnik@gmail.com>
2016-09-03 10:07:42 +0200

    - remove unused code

--- client/index.jsx
+++ client/index.jsx
@@ -3,10 +3,6 @@ import {render} from 'react-dom'
 import Router from './router.jsx';
 import 'babel-polyfill';
 
-$('body').bind('copy paste',function(e) {
-    e.preventDefault(); return false;
-});
-
 render(
     Router,
     document.getElementById('app')
