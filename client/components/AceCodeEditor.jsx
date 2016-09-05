22ca6dddeaffe38297746d62488b505443b4bc83
Jacek Spólnik <jacek.spolnik@gmail.com>
2016-09-05 17:57:11 +0200

    - remove block on pasting code - people want's that feature in

--- client/components/AceCodeEditor.jsx
+++ client/components/AceCodeEditor.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import 'brace';
 import AceEditor from 'react-ace';
-import {Alert} from 'react-bootstrap';
 
 import 'brace/mode/java';
 import 'brace/theme/tomorrow_night_eighties';
@@ -9,7 +8,6 @@ import 'brace/theme/tomorrow_night_eighties';
 export default class AceCodeEditor extends React.Component {
     constructor(props) {
         super(props);
-        this.state = {isCheating: false};
     }
     componentDidMount() {
         this.props.onSourceCodeChanged(this.props.sourceCode);
@@ -20,15 +18,6 @@ export default class AceCodeEditor extends React.Component {
             marginBottom: 10
         };
 
-        if (this.state.isCheating) {
-            return <div style={editorStyle}>
-                <Alert bsStyle="danger">
-                    <h4>Oh snap! You are cheating!</h4>
-                    <p>Please refresh page to continue and please stop pasting ready to go code. Be fair, as it is supposed to be fun and fair game.</p>
-                </Alert>
-            </div>;
-        }
-
         return <div style={editorStyle}>
             <AceEditor
                 mode="java"
@@ -36,9 +25,6 @@ export default class AceCodeEditor extends React.Component {
                 name="editor"
                 value={this.props.sourceCode}
                 onChange={this.props.onSourceCodeChanged}
-                onPaste={() => {
-                    this.setState({isCheating: true});
-                }}
                 height="350px"
                 width="100%"
                 editorProps={{$blockScrolling: true}}
