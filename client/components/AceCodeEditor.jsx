f64863dedd2a4d1960d5518c0db184b6c1a291b5
Jacek Spólnik <jacek.spolnik@gmail.com>
2016-09-07 06:48:20 +0200

    - add experimental fullScreen mode

--- client/components/AceCodeEditor.jsx
+++ client/components/AceCodeEditor.jsx
@@ -8,10 +8,23 @@ import 'brace/theme/tomorrow_night_eighties';
 export default class AceCodeEditor extends React.Component {
     constructor(props) {
         super(props);
+        this.state = {fullScreen: false};
+        this.fullScreenToggle = this.fullScreenToggle.bind(this);
     }
+
     componentDidMount() {
         this.props.onSourceCodeChanged(this.props.sourceCode);
     }
+
+    fullScreenToggle(editor, fullScreen) {
+        if (fullScreen == this.state.fullScreen)
+            return;
+
+        this.setState({fullScreen: fullScreen});
+        editor.setAutoScrollEditorIntoView(fullScreen);
+        editor.resize();
+    }
+
     render() {
         const editorStyle = {
             marginTop: 10,
@@ -20,14 +33,27 @@ export default class AceCodeEditor extends React.Component {
 
         return <div style={editorStyle}>
             <AceEditor
+                className={this.state.fullScreen ? "fullScreen" : ""}
                 mode="java"
                 theme="tomorrow_night_eighties"
                 name="editor"
                 value={this.props.sourceCode}
                 onChange={this.props.onSourceCodeChanged}
-                height="350px"
-                width="100%"
+                height={this.state.fullScreen ? "auto" : "350px"}
+                width={this.state.fullScreen ? "auto" : "100%"}
                 editorProps={{$blockScrolling: true}}
+                commands={[
+                    {
+                        name: "Show Fullscreen",
+                        bindKey: "F3",
+                        exec: (editor) => this.fullScreenToggle(editor, true)
+                    },
+                    {
+                        name: "Hide Fullscreen",
+                        bindKey: "Esc",
+                        exec: (editor) => this.fullScreenToggle(editor, false)
+                    },
+                ]}
             />
         </div>;
     }
