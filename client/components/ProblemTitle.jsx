601fcc19763e99233f410f9290dacd39e365d08a
Jacek Spólnik <jacek.spolnik@gmail.com>
2016-09-08 06:37:28 +0200

    - show problem ranking all the time

--- client/components/ProblemTitle.jsx
+++ client/components/ProblemTitle.jsx
@@ -7,18 +7,17 @@ import FontAwesome from './FontAwesome';
 const ProblemTitle = ({submissions, problem}) => {
     let submittedProblems = _.map(submissions, (submission) => submission.problemId);
 
-    if (_.includes(submittedProblems, problem.id)) {
-        return <PageHeader className="text-success">
-            {problem.title} <FontAwesome name="check-circle" />
-            <LinkContainer to={"problem/" + problem.id + "/rank"} className="pull-right">
-                <Button
-                    bsStyle="info"
-                ><FontAwesome name="trophy" lg={true}/> Problem Ranking</Button>
-            </LinkContainer>
-        </PageHeader>;
-    } else {
-        return <PageHeader>{problem.title}</PageHeader>;
-    }
+    let isProblemDone = _.includes(submittedProblems, problem.id);
+    let doneCheck = isProblemDone ? <FontAwesome name="check-circle" /> : null;
+
+    return <PageHeader className={isProblemDone ? "text-success" : ""}>
+        {problem.title} {doneCheck}
+        <LinkContainer to={"problem/" + problem.id + "/rank"} className="pull-right">
+            <Button
+                bsStyle="info"
+            ><FontAwesome name="trophy" lg={true}/> Problem Ranking</Button>
+        </LinkContainer>
+    </PageHeader>;
 };
 
 export default ProblemTitle;
