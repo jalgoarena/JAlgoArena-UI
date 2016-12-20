4ae0074fbc48c30c72338479958e2d1d1b615219
Jacek Spólnik <jacek.spolnik@gmail.com>
2016-12-20 06:31:04 +0100

    - add further fields for new problem page - add preview of function json to new problem page

--- client/pages/newProblem.jsx
+++ client/pages/newProblem.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import {Grid, Button, Col, PageHeader} from 'react-bootstrap';
 import {connect} from 'react-redux';
 import Markdown from 'react-remarkable';
+import JSONTree from 'react-json-tree';
 
 import FontAwesome from '../components/FontAwesome';
 
@@ -14,7 +15,21 @@ class NewProblem extends React.Component {
             level: "1",
             problemId: "TBD",
             timeLimit: 1,
-            memoryLimit: 32
+            memoryLimit: 32,
+            func: {
+                name: "fib",
+                return: {
+                    type: "java.lang.Long",
+                    comment: " N'th term of Fibonacci sequence"
+                },
+                parameters: [
+                    {
+                        name: "n",
+                        type: "java.lang.Integer",
+                        comment: "id of fibonacci term to be returned"
+                    }
+                ]
+            }
         });
     }
 
@@ -22,7 +37,7 @@ class NewProblem extends React.Component {
         return (
             <Grid>
                 <PageHeader className="text-center">Create New Problem</PageHeader>
-                <Col md={4}>
+                <Col md={5}>
                     <form>
                         <div className="form-group">
                             <input className="form-control" type="text" placeholder="Title" id="title"
@@ -64,18 +79,35 @@ class NewProblem extends React.Component {
                                       onChange={(e) => this.setState({description: e.target.value})}
                             />
                         </div>
+                        <div className="form-group">
+                            <input className="form-control" type="text" placeholder="Function Name" id="functionName"
+                                   value={this.state.func.name}
+                                   onChange={(e) => this.setState({
+                                       func: Object.assign({}, this.state.func, {name: e.target.value})
+                                   })}
+                            />
+                        </div>
+                        <div className="form-group">
+                            <input className="form-control" type="text" placeholder="Return Comment" id="returnComment"
+                                   value={this.state.func.return.comment}
+                                   onChange={(e) => this.setState({
+                                       func: Object.assign({}, this.state.func, {return: {type: this.state.func.return.type, comment: e.target.value}})
+                                   })}
+                            />
+                        </div>
                         <Button type="submit" bsStyle="success" className="pull-right" onClick={this.onSignUp}>
                             <FontAwesome name="book"/> Create Problem
                         </Button>
                     </form>
                 </Col>
-                <Col mdOffset={1} md={7} style={{backgroundColor: "#f1f1f1"}}>
+                <Col md={7} style={{backgroundColor: "#f1f1f1"}}>
                     <PageHeader className="text-center">{this.state.title}</PageHeader>
                     <h4>ID: {this.state.problemId}</h4>
                     <h4>Time Limit: {this.state.timeLimit}</h4>
                     <h4>Memory Limit: {this.state.memoryLimit}</h4>
                     <h4>Level: {this.state.level}</h4><br/><br/>
                     <Markdown source={this.state.description}/>
+                    <JSONTree data={this.state.func}/>
                 </Col>
             </Grid>
         );
