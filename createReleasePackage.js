c104695d19110751a5534d6581d9aa484089c733
Jacek Spólnik <jacek.spolnik@gmail.com>
2017-01-02 14:46:44 +0100

    - bug fixes - use global webpack instead of dependency one - which has problems on some environments to run - add missing assets dir to release package - remove postbuild step - it shouldn't run after every build - just on demand

--- createReleasePackage.js
+++ createReleasePackage.js
@@ -1,6 +1,6 @@
 var archiver = require('archiver');
 var fs = require('fs');
-var version = '1.0.2';
+var version = '1.0.3';
 
 if (process.env.TRAVIS_BUILD_NUMBER) {
     version = `${version}.${process.env.TRAVIS_BUILD_NUMBER}`;
