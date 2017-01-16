714df0f99fbd09b4eaf80d3ae36493a6b4398350
Jacek Spólnik <jacek.spolnik@gmail.com>
2017-01-16 11:49:27 +0100

    - bug fixes - deleting submissions results only in true/false - so we cannot refresh submissions just based on the status - remove additional step to fetch user, as it's done in one request now - fetch users doesn't require passing token, it's public endpoint - fix bug with reporting error on login/signup pages - fix redux dev tools - update all dependencies - remove short and float types, as they are rarely or not used in problems, can be always replaced by double and integer - fixes after code climate analysis - fixes after codacy analysis - clean css

--- createReleasePackage.js
+++ createReleasePackage.js
@@ -1,6 +1,6 @@
 var Archiver = require("archiver");
 var fs = require("fs");
-var version = "1.0.13";
+var version = "1.0.14";
 
 if (process.env.TRAVIS_BUILD_NUMBER) {
     version = `${version}.${process.env.TRAVIS_BUILD_NUMBER}`;
