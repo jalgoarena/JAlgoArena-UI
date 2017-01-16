3290d7ee71f14155b1436c2c740a5084824d6871
Jacek Spólnik <jacek.spolnik@gmail.com>
2017-01-16 06:50:47 +0100

    - add unit test for checking initial state of reducer

---
+++ test/reducers.spec.js
@@ -0,0 +1,27 @@
+import reducer from '../client/common/reducers';
+
+
+describe('problems reducer', () => {
+    it('should return the initial state', () => {
+        expect(
+            reducer(undefined, {})
+        ).toEqual(
+            {
+                sourceCode: null,
+                problems: [],
+                result: { statusCode: 'WAITING' },
+                showModal: false,
+                currentProblemId: null,
+                problemsFilter: 0,
+                programmingLanguage: 'java',
+                hideDoneProblems: false,
+                ranking: [],
+                problemRanking: [],
+                submissions: [],
+                submissionsFilter: 'ALL',
+                userAuthSession: { user: null, error: null, users: null },
+                routing: { "locationBeforeTransitions": null}
+            }
+        )
+    });
+});
