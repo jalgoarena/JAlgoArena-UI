6257c4d5fac665c56dc47cff71eda5f7572b6809
Jacek Spólnik <jacek.spolnik@gmail.com>
2017-01-16 22:28:30 +0100

    - feature enhancements - enable filtering of all possible statuses for submissions review

--- client/submissions/components/SubmissionsFilter.jsx
+++ client/submissions/components/SubmissionsFilter.jsx
@@ -9,7 +9,12 @@ const filterStyle = {
 const SubmissionsFilter = ({changeFilter, filter}) => (
     <Row>
         <Col md={11}>
-            <ButtonGroup className="pull-right" style={filterStyle} bsSize="large">
+            <ButtonGroup className="pull-right" style={filterStyle} >
+                <Button onClick={() => changeFilter('WRONG_ANSWER')} active={filter === 'WRONG_ANSWER'}>Wrong Answer</Button>
+                <Button onClick={() => changeFilter('COMPILE_ERROR')} active={filter === 'COMPILE_ERROR'}>Compile Error</Button>
+                <Button onClick={() => changeFilter('RUNTIME_ERROR')} active={filter === 'RUNTIME_ERROR'}>Runtime Error</Button>
+                <Button onClick={() => changeFilter('TIME_LIMIT_EXCEEDED')} active={filter === 'TIME_LIMIT_EXCEEDED'}>Time Limit</Button>
+                <Button onClick={() => changeFilter('MEMORY_LIMIT_EXCEEDED')} active={filter === 'MEMORY_LIMIT_EXCEEDED'}>Memory Limit</Button>
                 <Button onClick={() => changeFilter('ACCEPTED')} active={filter === 'ACCEPTED'}>Accepted</Button>
                 <Button onClick={() => changeFilter('RERUN_ACCEPTED')} active={filter === 'RERUN_ACCEPTED'}>Rerun Accepted</Button>
                 <Button onClick={() => changeFilter('ALL')} active={filter === 'ALL'}>All</Button>
