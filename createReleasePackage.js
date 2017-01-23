686653ffcb1bf746397e50bfe67b908a7abc87be
Jacek Spólnik <jacek.spolnik@gmail.com>
2017-01-23 15:37:44 +0100

    - new feature - allow user to see source code of their submissions - show properly max points for kotlin

--- createReleasePackage.js
+++ createReleasePackage.js
@@ -1,6 +1,6 @@
 var Archiver = require("archiver");
 var fs = require("fs");
-var version = "1.0.24";
+var version = "1.0.25";
 
 if (process.env.TRAVIS_BUILD_NUMBER) {
     version = `${version}.${process.env.TRAVIS_BUILD_NUMBER}`;
