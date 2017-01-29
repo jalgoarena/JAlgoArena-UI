a1026ec6c4d365ba11f94ae324688af2f910fa0c
Jacek Spólnik <jacek.spolnik@gmail.com>
2017-01-29 15:44:40 +0100

    - improvements - use React Bootstrap components for creating Form Components - simplify User Admin page - fix error message when cannot get raw problems - fix error message when auth service is unreachable

--- createReleasePackage.js
+++ createReleasePackage.js
@@ -1,6 +1,6 @@
 var Archiver = require("archiver");
 var fs = require("fs");
-var version = "1.0.30";
+var version = "1.0.31";
 
 if (process.env.TRAVIS_BUILD_NUMBER) {
     version = `${version}.${process.env.TRAVIS_BUILD_NUMBER}`;
