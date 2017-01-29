bd77b4b2d76a63fddb09448ed3c6ff8c442defe5
Jacek Spólnik <jacek.spolnik@gmail.com>
2017-01-29 15:42:19 +0100

    - improvements - use React Bootstrap components for creating Form Components - simplify User Admin page

--- client/users/pages/admin.jsx
+++ client/users/pages/admin.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import {findDOMNode} from 'react-dom';
 import _ from 'lodash';
-import {Grid, Button, Col, PageHeader} from 'react-bootstrap';
+import {Grid, Button, Col, PageHeader, Panel, FormGroup, FormControl, ControlLabel} from 'react-bootstrap';
 import {connect} from 'react-redux';
 import {regions, teams} from "../../config"
 
 import FontAwesome from '../../common/components/FontAwesome';
+import FieldGroup from '../../common/components/FieldGroup';
 import {fetchUsersWithAllData, updateUser} from "../actions";
 
 
@@ -57,86 +58,45 @@ class UsersAdmin extends React.Component {
                 <PageHeader className="text-center">Users ({users.length})</PageHeader>
                 <Col md={6}>
                     <form>
-                        <div className="panel panel-danger">
-                            <div className="panel-heading">
-                                <h3 className="panel-title">Choose user</h3>
-                            </div>
-                            <div className="panel-body">
-                                <select className="form-control" id="chosenUser"
-                                        value="admin"
-                                        onChange={(e) => this.setCurrentUser(e.target.value)}>
-                                    {userItems}
-                                </select>
-                            </div>
-                        </div>
-                        <div className="form-group">
-                            <label htmlFor="username" className="control-label">Username </label>
-                            <input className="form-control" type="text" placeholder="Username" id="username"
-                                   ref="username"
-                                   value={this.state.user.username}
-                                   readOnly="true"
-                            />
-                        </div>
-                        <div className="form-group">
-                            <label htmlFor="userId" className="control-label">User ID</label>
-                            <input className="form-control" type="text" placeholder="User ID" id="userId"
-                                   ref="userId"
-                                   value={this.state.user.id}
-                                   readOnly="true"
-                            />
-                        </div>
-                        <div className="form-group">
-                            <label htmlFor="email" className="control-label">Email</label>
-                            <input className="form-control" type="text" placeholder="Email" id="email"
-                                   ref="email"
-                                   value={this.state.user.email}
-                                   readOnly="true"
-                            />
-                        </div>
-                        <div className="form-group">
-                            <label htmlFor="role" className="control-label">Role</label>
-                            <input className="form-control" type="text" placeholder="Role" id="role"
-                                   ref="role"
-                                   value={this.state.user.role}
-                                   readOnly="true"
-                            />
-                        </div>
-                        <div className="form-group">
-                            <label htmlFor="region" className="control-label">Region</label>
-                            <select className="form-control" ref="region" id="region"
-                                    onChange={(e) => this.setState({
-                                        user: Object.assign({}, this.state.user, {
-                                            region: e.target.value
-                                        })
-                                    })}
-                            >
+                        <Panel header={<h3 className="panel-title">Choose user</h3>}>
+                            <select className="form-control" id="chosenUser"
+                                    value="admin"
+                                    onChange={(e) => this.setCurrentUser(e.target.value)}>
+                                {userItems}
+                            </select>
+                        </Panel>
+                        <FormGroup controlId="region">
+                            <ControlLabel>Region</ControlLabel>
+                            <FormControl componentClass="select"
+                                         value={this.state.user.region}
+                                         onChange={(e) => this.setState({
+                                             user: Object.assign({}, this.state.user, {
+                                                 region: e.target.value
+                                             })
+                                         })}>
                                 {regionOptions}
-                            </select>
-                        </div>
-                        <div className="form-group">
-                            <label htmlFor="team" className="control-label">Team</label>
-                            <select className="form-control" ref="team" id="team"
+                            </FormControl>
+                        </FormGroup>
+                        <FormGroup controlId="team">
+                            <ControlLabel>Team</ControlLabel>
+                            <FormControl componentClass="select"
+                                         value={this.state.user.team}
+                                         onChange={(e) => this.setState({
+                                             user: Object.assign({}, this.state.user, {
+                                                 team: e.target.value
+                                             })
+                                         })}>
+                                {teamOptions}
+                            </FormControl>
+                        </FormGroup>
+                        <FieldGroup id="password" type="password" placeholder="Change Password" label="Password"
+                                    value={this.state.user.password}
                                     onChange={(e) => this.setState({
                                         user: Object.assign({}, this.state.user, {
-                                            team: e.target.value
+                                            password: e.target.value
                                         })
                                     })}
-                            >
-                                {teamOptions}
-                            </select>
-                        </div>
-                        <div className="form-group">
-                            <label htmlFor="password" className="control-label">Password</label>
-                            <input className="form-control" type="text" placeholder="Password" id="password"
-                                   ref="password"
-                                   value={this.state.user.password}
-                                   onChange={(e) => this.setState({
-                                       user: Object.assign({}, this.state.user, {
-                                           password: e.target.value
-                                       })
-                                   })}
-                            />
-                        </div>
+                        />
                         <Button type="submit" bsStyle="success" block
                                 onClick={(e) => this.onCreateUser(e)}>
                             <FontAwesome name="send"/> Update
@@ -148,6 +108,7 @@ class UsersAdmin extends React.Component {
                         <h4>Username: {this.state.user.username}</h4>
                         <h4>ID: {this.state.user.id}</h4>
                         <h4>Email: {this.state.user.email}</h4>
+                        <h4>Role: {this.state.user.role}</h4>
                         <h4>Region: {this.state.user.region}</h4>
                         <h4>Team: {this.state.user.team}</h4>
                         <h4>Password: {this.state.user.password}</h4>
@@ -155,7 +116,7 @@ class UsersAdmin extends React.Component {
                     <div>
                         <PageHeader className="text-center">Result</PageHeader>
                         <span>
-                            {JSON.stringify(this.props.userUpdated)}
+                            {JSON.stringify(this.props.userUpdated || {})}
                         </span>
                     </div>
                 </Col>
