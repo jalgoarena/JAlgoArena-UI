0d2c7144dfce8351812d3257a9ff3b580ddfcd7b
Jacek Spólnik <jacek.spolnik@gmail.com>
2017-02-02 12:07:20 +0100

    - add unit tests for common actions

---
+++ test/common/commonActions.spec.js
@@ -0,0 +1,29 @@
+import * as types from "../../client/constants/ActionTypes";
+import * as actions from "../../client/common/actions";
+
+describe("actions", () => {
+    it("creates an action to close work in progress window", () => {
+        const expectedAction = {
+            type: types.CLOSE_WORK_IN_PROGRESS_WINDOW
+        };
+
+        expect(actions.closeWorkInProgressWindow()).toEqual(expectedAction);
+    });
+
+    it("creates an action to set error message", () => {
+        const expectedAction = {
+            type: types.SET_ERROR_MESSAGE,
+            error: "Error Message"
+        };
+
+        expect(actions.setErrorMessage("Error Message")).toEqual(expectedAction);
+    });
+
+    it("creates an action to clear error message", () => {
+        const expectedAction = {
+            type: types.RESET_ERROR_MESSAGE
+        };
+
+        expect(actions.clearErrorMessage()).toEqual(expectedAction);
+    });
+});
