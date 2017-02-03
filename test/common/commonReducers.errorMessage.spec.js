48239066083bf47475d95d1168cab0069eaf9a17
Jacek Spólnik <jacek.spolnik@gmail.com>
2017-02-03 16:13:29 +0100

    - add unit tests for errorMessage reducer

---
+++ test/common/commonReducers.errorMessage.spec.js
@@ -0,0 +1,23 @@
+import * as reducer from '../../client/common/reducers';
+import * as types from '../../client/constants/ActionTypes'
+
+describe('errorMessage reducer', () => {
+    it('should handle RESET_ERROR_MESSAGE', () => {
+        expect(
+            reducer.errorMessage("bad request",
+                {
+                    type: types.RESET_ERROR_MESSAGE
+                }
+            )
+        ).toEqual(null);
+
+        expect(
+            reducer.errorMessage(null,
+                {
+                    type: "ANY",
+                    error: "bad request"
+                }
+            )
+        ).toEqual("bad request");
+    });
+});
