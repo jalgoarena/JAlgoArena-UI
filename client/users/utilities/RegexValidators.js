e4bd3d2f1ccfa0dee83a52a4c9af8c9c9146be09
Jacek Spólnik <jacek.spolnik@gmail.com>
2017-02-05 13:08:16 +0100

    - remove comments

--- client/users/utilities/RegexValidators.js
+++ client/users/utilities/RegexValidators.js
@@ -1,29 +1,17 @@
 import config from '../../config'
 
-const displayNameRegex = /^[a-zA-Z\-_0-9]+$/; //alphanumerics, "-" and "_"
-const passwordRegex = /^[a-zA-Z0-9!@#$%^&*-]{6,}$/; // At least 6 characters
+const displayNameRegex = /^[a-zA-Z\-_0-9]+$/;
+const passwordRegex = /^[a-zA-Z0-9!@#$%^&*-]{6,}$/;
 
-/**
- *	Given a trimmed string, returns true if the string matches
- * a proper email format.
- */
 export function validateEmail(email) {
     return config.emailRegex.test(email);
 }
 
-/**
- *	Given a trimmed string, returns true if the string contains at
- * least one valid character (alphanumerics)
- */
 export function validateUserName(displayName) {
     return displayNameRegex.test(displayName);
 }
 
 
-/**
- *	Given a trimmed string, returns true if the string contains at
- * least 6 valid character (alphanumerics and !@#$%^&*)
- */
 export function validatePassword(password) {
     return passwordRegex.test(password);
 }
