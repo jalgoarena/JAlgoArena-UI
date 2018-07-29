e4939b647576dffac82f0358882262e8a42e2dc2
Jacek Spólnik <jacek.spolnik@gmail.com>
2018-07-29 21:29:51 +0200

    Create Profile page - add submissions calendar heatmap (for now hardcoded data)

--- client/common/components/RankingMenuItem.jsx
+++ client/common/components/RankingMenuItem.jsx
@@ -7,7 +7,7 @@ import MenuItem from './MenuItem';
 
 const RankingMenuItem = ({currentPath}: { currentPath: string }) => (
     <NavDropdown
-        title={<span><FontAwesome prefix="fas" name="trophy" lg={true} id="ranking-nav-dropdown"/> Ranking</span>}>
+        title={<span><FontAwesome prefix="fas" name="trophy" lg={true}/> Ranking</span>}>
         <MenuItem path="/userRanking" title="Users" icon="" prefix="" currentPath={currentPath}/>
         <MenuItem path="/teamRanking" title="Teams" icon="" prefix="" currentPath={currentPath}/>
         <MenuItem path="/regionRanking" title="Regions" icon="" prefix="" currentPath={currentPath}/>
--- client/users/pages/profile.jsx
+++ client/users/pages/profile.jsx
@@ -1,11 +1,37 @@
 import React from 'react';
-import {Grid, Col, Table, PageHeader} from 'react-bootstrap';
+import {Grid, Col, Well} from 'react-bootstrap';
 import {connect} from 'react-redux';
 import * as _ from "lodash";
 import User from "../domain/User";
 import Blockies from 'react-blockies';
+import FontAwesome from "../../common/components/FontAwesome";
+import CalendarHeatmap from 'react-calendar-heatmap';
 
 class Profile extends React.Component {
+
+    static githubClassForValue(value) {
+        if (!value || value.count === 0) {
+            return 'color-github-0';
+        }
+
+        if (value.count <= 12) {
+            return 'color-github-1';
+        }
+
+        if (value.count <= 24) {
+            return 'color-github-2';
+        }
+
+        if (value.count <= 36) {
+            return 'color-github-3';
+        }
+
+        return 'color-github-4';
+    }
+
+    static titleForValue(value) {
+        return value ? `${value.count} submissions on ${value.date}` : `0 submissions`;
+    }
 
     render() {
         let user = _.find(this.props.users,
@@ -21,6 +47,11 @@ class Profile extends React.Component {
             </Grid>;
         }
 
+        let startDate = new Date();
+        startDate.setMonth(startDate.getMonth() - 4);
+        let endDate = new Date();
+        endDate.setMonth(endDate.getMonth() + 4);
+
         return <Grid>
             <Col mdOffset={1} md={3}>
                 <Blockies
@@ -32,27 +63,44 @@ class Profile extends React.Component {
                     spotColor="#18bc9c"
                 />
                 <h2>{user.firstname} {user.surname}</h2>
+                <h4 style={{"color": "#979faf"}}>(@{user.username})</h4>
+                <hr/>
+                <Well bsSize="small">
+                    <h4 className="text-center"><FontAwesome prefix="fas" name="globe"/> {user.region}</h4>
+                </Well>
+                <Well bsSize="small">
+                    <h4 className="text-center"><FontAwesome prefix="fas" name="users"/> {user.team}</h4>
+                </Well>
             </Col>
             <Col md={7}>
-                <PageHeader>Profile</PageHeader>
-                <Table striped bordered condensed hover responsive>
-                    <thead>
-                    <tr>
-                        <th>User ID</th>
-                        <th>User Name</th>
-                        <th>Region</th>
-                        <th>Team</th>
-                    </tr>
-                    </thead>
-                    <tbody>
-                    <tr>
-                        <td>{user.id}</td>
-                        <td>{user.username}</td>
-                        <td>{user.region}</td>
-                        <td>{user.team}</td>
-                    </tr>
-                    </tbody>
-                </Table>
+                <h4>Solved Problems</h4>
+                <hr/>
+                <h4 className="text-center" style={{"color": "#979faf"}}>{user.username} has not solved any problems yet.</h4>
+                <br/>
+                <h4>Rating</h4>
+                <hr/>
+                <br/>
+                <h4>Submissions</h4>
+                <hr/>
+                <CalendarHeatmap
+                    startDate={startDate}
+                    endDate={endDate}
+                    values={[
+                        { date: '2018-07-07', count: 1 },
+                        { date: '2018-07-08', count: 4 },
+                        { date: '2018-07-09', count: 9 },
+                        { date: '2018-07-10', count: 14 },
+                        { date: '2018-07-11', count: 19 },
+                        { date: '2018-07-12', count: 24 },
+                        { date: '2018-07-13', count: 29 },
+                        { date: '2018-07-14', count: 34 },
+                        { date: '2018-07-15', count: 50 },
+                        { date: '2018-07-16', count: 70 },
+                    ]}
+                    classForValue={Profile.githubClassForValue}
+                    titleForValue={Profile.titleForValue}
+                    tooltipDataAttrs={{ 'data-toggle': 'tooltip' }}
+                />
             </Col>
         </Grid>;
     }
