7ecb151c6c350b83cfb903fd0f04f021dd786a69
Jacek Spólnik <jacek.spolnik@gmail.com>
2018-07-31 21:41:53 +0200

    - update version

--- createReleasePackage.js
+++ createReleasePackage.js
@@ -1,6 +1,6 @@
 const Archiver = require("archiver");
 const fs = require("fs");
-let version = "2.4.7";
+let version = "2.4.8";
 
 if (process.env.TRAVIS_BUILD_NUMBER) {
     version = `${version}.${process.env.TRAVIS_BUILD_NUMBER}`;
