a253ad21fc9e451792f3334c6274bf16d5756997
Jacek Spólnik <spolnik@users.noreply.github.com>
2018-08-09 09:34:14 +0200

    Update SaveButton.jsx

--- client/problems/components/SaveButton.jsx
+++ client/problems/components/SaveButton.jsx
@@ -14,7 +14,7 @@ type SaveButtonInputType = {
 const SaveButton = ({sourceCode, savedSourceCode, problemId, onSave}: SaveButtonInputType) => (
     <button
         style={{width: '200px'}}
-        className="btn btn-lg btn-success pull-right"
+        className="btn btn-lg btn-primary pull-right"
         disabled={sourceCode === savedSourceCode}
         onClick={() => onSave(sourceCode, problemId)}>
         <FontAwesome prefix="fas" name="save"/> Save
