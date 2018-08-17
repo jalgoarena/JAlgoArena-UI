0ba989ba02e61419276ee3b4c314d344d0e79f54
Jacek Spólnik <jacek.spolnik@gmail.com>
2018-08-17 21:55:59 +0200

    Babel/JS to TypeScript migration - final commit :))

--- client/index.tsx
+++ client/index.tsx
@@ -3,7 +3,7 @@ require('es6-promise').polyfill();
 import 'isomorphic-fetch';
 import * as React from 'react';
 import * as ReactDOM from 'react-dom'
-import Router from './router.jsx';
+import Router from './router';
 
 import './assets/app.css';
 
--- client/vendors.ts
+++ client/vendors.ts
@@ -1,10 +1,10 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-require("typeface-roboto");
-require("@fortawesome/fontawesome-free/css/all.css");
-require("bootstrap/dist/css/bootstrap.min.css");
-require("react-bootstrap-table/css/react-bootstrap-table.css");
-require("jquery");
-require("popper.js");
-require("bootstrap");
+import "typeface-roboto";
+import "@fortawesome/fontawesome-free/css/all.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "react-bootstrap-table/css/react-bootstrap-table.css";
+import "jquery";
+import "popper.js";
+import "bootstrap";
 //# sourceMappingURL=vendors.js.map
