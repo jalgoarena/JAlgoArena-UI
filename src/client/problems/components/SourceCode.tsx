34690f9f86ec5dff7860ad2fae664349f4dbf933
Jacek Spólnik <jacek.spolnik@gmail.com>
2018-10-07 13:18:01 +0200

    Fix pair source code view

--- src/client/problems/components/SourceCode.tsx
+++ src/client/problems/components/SourceCode.tsx
@@ -104,6 +104,11 @@ const PairSourceCode = (props: SourceCodeProps) => (
         sourceCode={`public class Pair {
     public Object first;
     public Object second;
+
+    public Pair(Object first, Object second) {
+        this.first = first;
+        this.second = second;
+    }
 }`}
         show={props.show}
         onHide={props.onHide}
